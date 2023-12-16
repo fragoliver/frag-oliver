d0b4771e5075d3132fff7d9f31570c74205c4292
Chris Dorward <chrisdorward@canarycare.co.uk>
2023-12-17 00:35:04 +0100

    .

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,18 @@
 import {onRequest} from "firebase-functions/v2/https";
-import {CanaryShape} from "./types";
+import {APIShape} from "./types";
 import express, {Request, Response} from "express";
 import cors from "cors";
 import moment from "moment";
 
-const version = "2.0.3";
+const version = "2.0.4";
 const app = express();
 app.use(cors({credentials: true}));
 
-const respond = (req: Request, res: Response, data: CanaryShape) => {
+const respond = (req: Request, res: Response, data: APIShape) => {
   const {output} = data;
   const unixEpoch = Date.now();
   const api = {
-    app: "Frag Oliver API",
+    app: "frag-oliver-api",
     version: `${version}`,
     time: moment(unixEpoch).add(2, "hours").format("MMMM Do YYYY, h:mm:ssa"),
     response: output,
